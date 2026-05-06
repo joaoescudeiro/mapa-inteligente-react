0b8261f224b540057604232c1031e28d585efa53
João Vitor Escudeiro <joaovitor.escudeiro01@gmail.com>
2026-05-06 15:44:48 -0300

    Implementando funcao de gravar cadastro no firebase

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1e7x486lSoqfbTxWV7UVaVJGhBYQpkfw",
+    authDomain: "mapa-inteligente-bad19.firebaseapp.com",
+    projectId: "mapa-inteligente-bad19",
+    storageBucket: "mapa-inteligente-bad19.firebasestorage.app",
+    messagingSenderId: "202287431904",
+    appId: "1:202287431904:web:f17ecb399b177cebc35085"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;    
--- src/telas/TelaCadastro.js
+++ src/telas/TelaCadastro.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
+import firebase from '../services/firebaseConfig';
+import { Alert } from 'react-native';
+
 
 export default function TelaCadastro() {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+
+    function gravar() {
+        const emailFormatado = email.toLowerCase();
+
+        firebase.auth()
+            .createUserWithEmailAndPassword(emailFormatado, senha)
+            .then(() => {
+                Alert.alert("Sucesso", "Usuário cadastrado com sucesso!");
+            })
+            .catch(error => {
+                const errorCode = error.code;
+
+                if (errorCode === "auth/email-already-in-use")
+                    Alert.alert("Erro", "Esse e-mail já está em uso");
+                else if (errorCode === "auth/weak-password")
+                    Alert.alert("Erro", "Senha fraca (mínimo 6 caracteres)");
+                else if (errorCode === "auth/invalid-email")
+                    Alert.alert("Erro", "Formato de e-mail inválido");
+                else
+                    Alert.alert("Erro", error.message);
+            });
+    }
 
     return (
         <View style={styles.container}>
@@ -30,7 +55,7 @@ export default function TelaCadastro() {
                 style={styles.input}
             />
 
-            <Button title="Entrar" onPress={() => { }} />
+            <Button title="Cadastrar" onPress={gravar} />
         </View>
     );
 }
